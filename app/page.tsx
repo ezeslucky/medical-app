849d5ddaa9399bb56d5c7c5161496cafb3527b03
Lucky <lucky123@gmail.com>
2024-10-21 20:20:09 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className=" text-3xl flex min-h-screen justify-center items-center">
+      <h2 className=" font-bold">medical</h2>
+    </section>
+  );
+}
